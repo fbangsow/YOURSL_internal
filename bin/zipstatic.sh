d1a85c10252e2ac808c4e79c6a12d7359e59e0cf
Sven Kretschmann <svenkretschmann@adsl-172-10-2-248.dsl.sndg02.sbcglobal.net>
2016-01-26 15:18:12 +0100

    Adding a script to package a directory in the static resources and upload it to Salesforce. This requires an existing static resource with the directorys name and Sublime 3 with Mavensmate set up.

---
+++ bin/zipstatic.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+ROOTDIR=$(cd `dirname "${0}"`/.. && pwd)
+RESOURCESDIR=${ROOTDIR}/src/staticresources
+PROJECTNAME=`basename $ROOTDIR`
+MM_WORKSPACE=`dirname $ROOTDIR`
+
+TARGET=$1
+
+if [ -z "$TARGET" -o ! -d "$RESOURCESDIR/$TARGET" ]
+then
+	echo "Invalid static resource container $TARGET"
+	exit 1
+fi
+
+echo "Start watching for changes in $TARGET"
+
+MM_LOGFILE="$RESOURCESDIR/$TARGET/mm.log"
+
+fswatch -0 $RESOURCESDIR/$TARGET | while read -d "" event
+do
+	if [ "${event}" != "${MM_LOGFILE}" ]
+	then
+		echo "File changed: ${event}"
+
+		cd $RESOURCESDIR/$TARGET
+		if [ -f "${MM_LOGFILE}" ]
+		then
+			rm "${MM_LOGFILE}"
+		fi
+
+		echo "Compressing files in $TARGET"
+
+		zip -r ../${TARGET}.resource *
+
+		echo "Uploading archive to SalesForce"
+
+		~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/MavensMate/mm/mm -o compile -c SUBLIME_TEXT_3 <<< '{"project_name": "'${PROJECTNAME}'", "workspace": "'${MM_WORKSPACE}'", "files": ["'${RESOURCESDIR}/${TARGET}'.resource"]}'
+
+		echo
+	fi
+done
